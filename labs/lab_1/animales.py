299b7319c6807a84e54692cbedee5641aa75f470
Benjamín Fuentes R <fuentesbenjamin11@gmail.com>
2026-03-18 20:38:10 -0300

    fix: Se arregla formato

--- labs/lab_1/animales.py
+++ labs/lab_1/animales.py
@@ -4,11 +4,11 @@ def animales(input):
     Como hace el animalito?
     '''
 
-    if input.lower() == 'gato':
-        print('miau')
-    elif input.lower() == 'perro':
-        print('guau')
-    elif input.lower() == 'fox':
-        print('https://www.youtube.com/watch?v=jofNR_WkoCE')
+    if input.lower() == "gato":
+        print("miau")
+    elif input.lower() == "perro":
+        print("guau")
+    elif input.lower() == "fox":
+        print("https://www.youtube.com/watch?v=jofNR_WkoCE")
     else:
-        raise ValueError('animal no reconocido :(')
+        raise ValueError("animal no reconocido :(")
